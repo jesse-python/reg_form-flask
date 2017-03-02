6d54a841d0f92e7077512b3a26453833246b8456
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-03-02 13:26:57 -0800

    added date of birth

--- reg.py
+++ reg.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request, flash
 import re, codecs
 EMAIL_REGEX = re.compile(r'^[a-za-z0-9\.\+_-]+@[a-za-z0-9\._-]+\.[a-za-z]*$')
+DATE_REGEX = re.compile(r'^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$')
 app = Flask(__name__)
 app.secret_key = 'ThisIsSecret'
 
@@ -64,6 +65,14 @@ def result():
     elif len(c_password) < 8:
         flash("Confirm password cannot be less than 8 characters ")
         submit = False
+
+    if len(request.form['date_of_birth']) < 1:
+        flash("Date of birth cannot be blank!")
+        submit = False
+    elif not DATE_REGEX.match(request.form['date_of_birth']):
+        flash("Date of birth format not correct")
+        submit = False
+
 
     if (submit == True):
         flash("Thanks for submitting your information!")
