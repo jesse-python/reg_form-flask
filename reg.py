2f8ec3e527f00ebf08a1b3a117deaab790e2f689
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-03-02 13:22:53 -0800

    finished

--- reg.py
+++ reg.py
@@ -8,11 +8,65 @@ app.secret_key = 'ThisIsSecret'
 def index():
     return render_template('index.html')
 
-@app.route('/process' methods=['POST'])
+@app.route('/process', methods=['POST'])
 def result():
-    if len(request.form['email'] < 1:
-        flash('Email cannot be blank!')
 
+    submit = True
+
+    email = str(request.form['email'])
+    if len(email) < 1:
+        flash('Email cannot be blank!')
+        submit = False
+    elif not EMAIL_REGEX.match(email):
+        flash('Invalid email, try again')
+        submit = False
+
+    first_name = str(request.form['first_name'])
+    if len(first_name) < 1:
+        flash('First name cannot be blank')
+        submit = False
+    elif len(first_name) > 1:
+        if not str.isalpha(first_name):
+            flash("First name can only contain alphabetic characters!")
+            submit = False
+
+    last_name = str(request.form['last_name'])
+    if len(last_name) < 1:
+        flash("Last name cannot be blank!")
+        submit = False
+    elif len(last_name) > 1:
+        if not str.isalpha(last_name):
+            flash("Last name can only contain alphabetic characters!")
+            submit = False
+
+    password = str(request.form['password'])
+    c_password = str(request.form['c_password'])
+
+    if len(password) < 1:
+        flash("Password cannot be blank!")
+        submit = False
+    elif len(password) < 8:
+        flash('Password cannot be less than 8 characters')
+        submit = False
+    elif password != c_password:
+        flash("Password does not match your confirmed password")
+        submit = False
+    elif not str.isalnum(password):
+        flash("Password needs to be alphanumeric!")
+        submit = False
+    elif str.islower(password):
+        flash("Password needs to have at least one uppercase letter!")
+        submit = False
+
+    if len(c_password) < 1:
+        flash("Confirm password cannot be blank!")
+        submit = False
+    elif len(c_password) < 8:
+        flash("Confirm password cannot be less than 8 characters ")
+        submit = False
+
+    if (submit == True):
+        flash("Thanks for submitting your information!")
 
     return redirect('/')
 app.run(debug=True)
